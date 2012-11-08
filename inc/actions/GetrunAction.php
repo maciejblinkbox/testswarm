3fad87e11c89ee995795763b1800f983b33cb6d1
Maciej Borzecki <maciej@blinkbox.com>
2012-11-08 16:02:00 +0000

    Server side timeout: - GetrunAction.php: set expected_update when runresult is created

--- inc/actions/GetrunAction.php
+++ inc/actions/GetrunAction.php
@@ -77,15 +77,18 @@ class GetrunAction extends Action {
 			if ( $row->run_url && $row->job_name && $row->run_name ) {
 				// Create stub runresults entry
 				$storeToken = sha1( mt_rand() );
+				$timeoutMargin = 10;	// 10 seconds margin
+				$expected_update = $now + $timeoutMargin;
 				$isInserted = $db->query(str_queryf(
 					'INSERT INTO runresults
-					(run_id, client_id, status, store_token, updated, created)
-					VALUES(%u, %u, 1, %s, %s, %s);',
+					(run_id, client_id, status, store_token, updated, created, expected_update)
+					VALUES(%u, %u, 1, %s, %s, %s, %u);',
 					$runID,
 					$clientID,
 					sha1( $storeToken ),
 					swarmdb_dateformat( SWARM_NOW ),
-					swarmdb_dateformat( SWARM_NOW )
+					swarmdb_dateformat( SWARM_NOW ),
+					swarmdb_dateformat( $expected_update )
 				));
 				$runresultsId = $db->getInsertId();
 				if ( !$isInserted || !$runresultsId ) {
